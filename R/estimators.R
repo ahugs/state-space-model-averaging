82d621773cc5eee4d7a5c0399f33e7689d319c65
Adriana Hugessen <hugessen.adriana@gmail.com>
2022-03-18 22:59:56 -0600

    Stoffer estimator

--- R/estimators.R
+++ R/estimators.R
@@ -1,6 +1,8 @@
 library(bsts)
 library(dlm)
 library(parallel)
+library(astsa)
+
 
 BaseEstimator <- setRefClass("BaseEstimator",
         fields = list(
@@ -96,7 +98,59 @@ StofferEstimator <- setRefClass("StofferEstimator",
     #TODO: Implement 
     methods = list (
       fit_single_frequency = function(freq, y, lags) {
-        stop("Function 'fit_single_frequency' must be implemented")
+        N = length(y)
+        stoffer_x = rep(NA, N)
+        stoffer_models = list()
+        
+        tryCatch(
+          for(i in 1:freq){
+            y_sub = ts(y[seq(i, N, freq)])
+            init.par = c(rep(0, lags), 0.5^2, 0.5^2)
+          
+            
+            A = matrix(c(1, rep(0, lags-1)), 1, lags)
+            Sigma0 = diag(100, lags)
+            mu0 = matrix(0, lags)
+            # Function to evaluate the likelihood
+            Linn=function(para){
+              phi = t(rbind(para[1:lags],diag(lags)[-lags,] ))
+              sig_evol = matrix(0, lags, lags)
+              sig_evol[1,1] = para[lags+1]
+              sig_obs = para[lags+2]
+              kf = Kfilter0(length(y_sub), y_sub, A, mu0=mu0, Sigma0, phi,  sig_evol, sig_obs)
+              return(kf$like)
+            }
+            
+            # Estimation
+            est = optim(init.par, Linn, gr=NULL, method="BFGS", hessian=TRUE)
+            if(est$convergence != 0){
+              stop("Failed to converge")
+            }
+            stoffer_models[[i]] = est
+            
+            phi = est$par[1:lags]
+            phi = t(rbind(phi,diag(lags)[-lags,] ))
+            sig_evol = matrix(0, lags, lags)
+            sig_evol[1,1] = est$par[lags+1]
+            sig_obs = est$par[lags+1]
+            states = Ksmooth0(length(y_sub), y_sub, A, mu0=mu0, Sigma0, phi,  sig_evol, sig_obs)$xs
+            
+            stoffer_x[seq(i, N, freq)] = states
+          },
+          error=function(e) {print(e)}
+        )
+        return(list(x=stoffer_x, models=stoffer_models))
       }
     )
+)
+
+
+NoModelEstimator <- setRefClass("DLMEstimator",
+    contains = "BaseEstimator",
+    methods = list (
+      fit_single_frequency = function(freq, y, lags) {
+        return(list(x=y, models=list()))
+      }
+    )
+
 )
